4e44a464a200b2b826495e241dd3ee24652fa9e6
angelohh <angelohh29.04@gmail.com>
2023-07-30 16:11:19 -0400

    Commented the scraper of manga bot.

--- manga_bot/lectormo/cloud.py
+++ manga_bot/lectormo/cloud.py
@@ -12,26 +12,39 @@ from manga_bot.lectormo.scraper import LectorMO
 
 
 def split_image(url: str, max_width: int, max_height: int, min_height: int) -> list[bytes]:
+    """ Download the image and split it into small parts. """
+    # Disable SSL warnings (the certificated is expired).
     disable_warnings(exceptions.InsecureRequestWarning)
+    # Get the image and their dimensions.
     content = LectorMO().scraper.get(url, verify=False)
     images, content = [], content.content
     image = Image.open(BytesIO(content))
     width, height = image.size
+    # Get the relative size and the number of times to
+    # cut the image.
     value = height * (max_width * 100 / width) / 100
     new_height = math.ceil((height - value) + max_height)
 
+    # If the max height is less that the current height
+    # return the image without cuts.
     if max_height > height:
         buffer = io.BytesIO()
         image.save(buffer, format='JPEG')
         return [buffer.getvalue()]
 
+    # The big problem, split the image and round the number
+    # of pixels per new image.
     for y in range(math.ceil(value / new_height)):
         current, buffer = new_height * y, io.BytesIO()
         next_height = new_height * (y + 1)
+        # If the current height and the image height are
+        # equals break the iteration.
         if height == current or (height - current) < 0:
             break
+        # If the next image is too small merge it with this.
         if (height - next_height) <= min_height:
             next_height += height - next_height
+        # Cut the image and save it in an array buffer.
         img = image.crop((0, current, width, next_height))
         img.save(buffer, format='JPEG')
         images.append(buffer.getvalue())
@@ -41,6 +54,7 @@ def split_image(url: str, max_width: int, max_height: int, min_height: int) -> l
 def upload_images(images: list[str]) -> list[str]:
     images_url, url = [], 'https://telegra.ph/upload'
     for img in images:
+        # Split the image and upload to Telegraph cloud.
         for image in split_image(img, 732, 690, 80):
             data = requests.post(url, files={'file': image})
             data = data.json()[0]['src']
@@ -49,17 +63,25 @@ def upload_images(images: list[str]) -> list[str]:
 
 
 def upload_chapter(title: str, author: str, images: list[str], per_page: int):
+    # Save the images in html format and create a
+    # Telegraph profile.
     tags = [f'<img src="{url}">' for url in images]
     telegraph, urls = Telegraph(), []
     telegraph.create_account(short_name=author)
+
+    # Iterate the images in groups.
     for i in range(0, len(images), per_page)[::-1]:
         content = '\n'.join(tags[i:i+per_page])
+        # If not the first page add a new tag for redirect
+        # to the next page.
         if len(urls) != 0:
             total_pages = math.ceil(len(images) / per_page)
             page_number = total_pages - len(urls) + 1
             text = f'Next Page {page_number} / {total_pages}'
             content += f'<a href="{urls[-1]}">{text}</a>'
+        # Create the page and save.
         args = dict(title=title, html_content=content)
         urls.append(telegraph.create_page(**args)['url'])
+        # Wait a few minutes to avoid overloading the cloud.
         time.sleep(1)
     return urls
--- manga_bot/lectormo/scraper.py
+++ manga_bot/lectormo/scraper.py
@@ -17,10 +17,13 @@ class LectorMO:
         self.scraper.headers['User-Agent'] = agent
 
     def _get_manga(self, raw_text: BeautifulSoup):
+        # Extract background image URL from the style script.
         script = raw_text.find('style').text
         expression = r"(?P<url>https?://[^\s]+)'\);"
         background = re.search(expression, script)
         background = background.group("url")
+
+        # Extract manga title, demography, book type, and score.
         title = raw_text.find('h4', 'text-truncate').text
         demography = {'class': 'demography'}
         demography = raw_text.find('span', demography).text
@@ -28,6 +31,9 @@ class LectorMO:
         book_type = raw_text.find('span', book_type).text
         score = {'class': 'score'}
         score = float(raw_text.find('span', score).text)
+
+        # Create and return a dictionary containing the manga
+        # information.
         return {
             'id': raw_text['href'].split('/')[-2],
             'title': title, 'book-type': book_type,
@@ -37,49 +43,64 @@ class LectorMO:
 
     def get_pill(self, tab_name: str, content: bytes) -> list:
         soup = BeautifulSoup(content, 'html.parser')
+        # Find the main container for the tab content.
         container_data = {'class': 'container-fluid'}
         container = soup.find('main', container_data)
+        # Find the specific tab element based on the given name.
         tab = container.find('div', {'id': f'pills-{tab_name}'})
         if tab is None: return []
         manga_list = tab.find_all('a')[:-1]
+        # Create a list of mangas information if the tab is not
+        # None.
         return [self._get_manga(manga) for manga in manga_list]
 
     def search(self, query: str) -> list:
         query = query.replace(' ', '+')
+        # Create the URL to search for manga based on the query.
         url = f'{self.base_url}library?_pg=1&title={query}'
         content = self.scraper.get(url).content
+        # Find the main container for the search results.
         soup = BeautifulSoup(content, 'html.parser')
         container = soup.find_all('div', {'class': 'row'})[1]
         content = container.find('div', {'class': 'col-xl-9'})
         row = content.find_all('div', {'class': 'row'})[1]
+        # Create a list of mangas information.
         return [self._get_manga(a) for a in row.find_all('a')]
 
     def _get_hashtag(self, url):
+        # Use the scraper to get the html content and
+        # parse it.
         content = self.scraper.get(url).content
         soup = BeautifulSoup(content, 'html.parser')
         card = soup.find('div', {'class': 'clearfix'})
+        # Get a list of hashtags present in the url.
         return [a.text for a in card.find_all('a')]
 
     def get_memes(self, content: bytes):
         soup = BeautifulSoup(content, 'html.parser')
+        # Find the container that holds the memes.
         container_data = {'class': 'container-fluid'}
         container = soup.find('main', container_data)
+        # Find all the memes within the container.
         memes_container = container.find_all('div', {'class': 'row'})[16]
         memes_data = {'class': 'col-12 col-md-4 mt-2'}
-        memes = memes_container.find_all('div', memes_data)
+        # Return a list of dictionaries, each representing a meme.
         return [{
             'url': meme.find('a')['href'],
             'img': meme.find('img')['src'],
             'hashtags': self._get_hashtag(meme.find('a')['href'])
-        } for meme in memes]
+        } for meme in memes_container.find_all('div', memes_data)]
 
     def get_scan(self, chapter: BeautifulSoup):
         scan_list = []
+        # Loop through each 'li' element within the container.
         for scan in chapter.find_all('li'):
+            # Extract the date and convert it into date object.
             date = scan.find('span', {'class': 'badge'})
             date = date.text.strip()
             date = [int(i) for i in date.split('-')]
             date = datetime.date(*date).strftime('%d-%m-%Y')
+            # Create a dictionary and append to it to scan_list.
             scan_list.append({
                 'scan-name': scan.find('a').text, 'date': date,
                 'url': scan.find('a', {'class': 'btn'})['href']
@@ -89,14 +110,18 @@ class LectorMO:
     def one_shot(self, raw_text: BeautifulSoup):
         data = {'class': 'chapter-list-element'}
         container = raw_text.find('div', data)
+        # Get the one_shot chapter (nothing to say).
         li = self.get_scan(container)
         return dict(number='one shot', title='', scans=li)
 
     def get_chapters(self, raw_text: BeautifulSoup):
+        # Get the chapter list and the scans in each of this.
         container = raw_text.find('div', {'id': 'chapters'})
         chapters_data = {'class': 'upload-link'}
+        # If the manga is a one-shot return the only chapter.
         if container is None: return [self.one_shot(raw_text)]
         chapters: list = container.find_all('li', chapters_data)
+        # Loop through each chapter in the chapter list.
         for index, chapter in enumerate(chapters):
             title = chapter.find('a').text.strip()
             number = title.split(' ')[1]
@@ -109,25 +134,29 @@ class LectorMO:
         content = self.scraper.get(url).content
         soup = BeautifulSoup(content, 'html.parser')
         title_class = {'class': 'element-title'}
+        # If the title is not found, raise an exception (too many requests).
         exception = self.exceptions.too_many_request
         title = soup.find('h1', title_class)
         if title is None: raise exception()
+        # Find the manga image url, title, subtitle, synopsis, genres and
+        # score using their respective tags.
+        image = soup.find('img', {'class': 'book-thumbnail'})
         title = title.text.strip().replace('\n', ' ')
         subtitle_class = {'class': 'element-subtitle'}
         subtitle = soup.find('h2', subtitle_class)
-        subtitle = subtitle.text
+        subtitle, image = subtitle.text, image['src']
         synopsis_class = {'class': 'element-description'}
         synopsis = soup.find('p', synopsis_class).text
         genres = [h6.text for h6 in soup.find_all('h6')]
         score = soup.find('div', {'class': 'score'}).text.strip()
         return {
-            'id': url.split('/')[-2], 'title': title,
-            'image': soup.find('img', {'class': 'book-thumbnail'})['src'],
+            'id': url.split('/')[-2], 'title': title, 'image': image,
             'subtitle': subtitle, 'genres': genres, 'score': score,
             'synopsis': synopsis, 'chapters': self.get_chapters(soup)
         }
 
     def get_images(self, url):
+        # Get all images in one page.
         url = self.scraper.get(url).url
         mode = url.split('/')[-1]
         url = url.replace(mode, 'cascade')
@@ -135,8 +164,13 @@ class LectorMO:
         content = self.scraper.get(url).content
         soup = BeautifulSoup(content, 'html.parser')
         image_data = {'class': 'viewer-img'}
+
+        # List all images and save the urls.
         images = soup.find_all('img', image_data)
         images = [img['data-src'] for img in images]
         image = []
+
+        # Filter the duplicated images without change the
+        # order.
         [image.append(i) for i in images if i not in image]
         return image
