b335e653f757949a84f14ab5fb8bd21b7109626d
angelohh <angelohh29.04@gmail.com>
2023-09-18 11:31:22 -0400

    Fixed the method of separating chapter images.

--- manga_bot/telegraph/manager.py
+++ manga_bot/telegraph/manager.py
@@ -11,6 +11,28 @@ from urllib3 import disable_warnings, exceptions
 from manga_bot.lectormo.scraper import LectorMO
 
 
+def get_coordinates(image_size: tuple[int, int], default_size: tuple[int, int], min_height: int):
+    width, height = image_size
+    coordinates = []
+    # Calculate the scaled size based on the image's aspect ratio.
+    scaled_size = math.ceil((height / width) * default_size[0])
+    max_size = math.ceil(height / (scaled_size / default_size[1]))
+    # Loop through the image coordinates vertically in chunks.
+    for current_y in range(0, height, max_size):
+        next_y = current_y + max_size
+        # Ensure that the next chunk doesn't exceed the image height.
+        next_y = next_y if height - next_y >= 0 else height
+        # Check if the next image chunk is too small to split.
+        if height - next_y != 0 and height - next_y <= min_height:
+            j = math.ceil((height - current_y) / 2)
+            coordinates.append((current_y, j + current_y))
+            coordinates.append((j + current_y, height))
+            break
+        # Append the current chunk's coordinates to the list.
+        coordinates.append((current_y, next_y))
+    return coordinates
+
+
 def split_image(url: str, max_width: int, max_height: int, min_height: int) -> list[bytes]:
     """ Download the image and split it into small parts. """
     # Disable SSL warnings (the certificated is expired).
@@ -20,30 +42,12 @@ def split_image(url: str, max_width: int, max_height: int, min_height: int) -> l
     images, content = [], content.content
     image = Image.open(BytesIO(content))
     width, height = image.size
-    # Get the relative size and the number of times to
-    # cut the image.
-    value = height * (max_width * 100 / width) / 100
-    new_height = math.ceil((height - value) + max_height)
 
-    # If the max height is less that the current height
-    # return the image without cuts.
-    if max_height > height:
+    coordinates = get_coordinates(image.size, (max_width, max_height), min_height)
+    # If the max height is less that the current height set the image without cuts
+    coordinates = [image.size] if max_height >= height else coordinates
+    for current, next_height in coordinates:
         buffer = io.BytesIO()
-        image.save(buffer, format='JPEG')
-        return [buffer.getvalue()]
-
-    # The big problem, split the image and round the number
-    # of pixels per new image.
-    for y in range(math.ceil(value / new_height)):
-        current, buffer = new_height * y, io.BytesIO()
-        next_height = new_height * (y + 1)
-        # If the current height and the image height are
-        # equals break the iteration.
-        if height == current or (height - current) < 0:
-            break
-        # If the next image is too small merge it with this.
-        if (height - next_height) <= min_height:
-            next_height += height - next_height
         # Cut the image and save it in an array buffer.
         img = image.crop((0, current, width, next_height))
         img.save(buffer, format='JPEG')
